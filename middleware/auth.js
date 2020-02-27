a3f038365061403032a2de46608b9208bc2a4278
HungThinh0710 <nhtnokia@gmail.com>
2020-02-27 11:22:18 +0700

    Fixed donot response when token is missing

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,9 +2,9 @@ const jwt = require("jsonwebtoken")
 const User = require("../models/User")
 
 const auth = async(req, res, next) => {
-    const token = req.get('Authorization').replace('Bearer ', '')
     // const data = jwt.verify(token, process.env.JWT_KEY)
     try {
+        const token = req.get('Authorization').replace('Bearer ', '')
         const data = jwt.verify(token, process.env.JWT_KEY );    
         const user = await User.findOne({ _id: data._id, 'tokens.token': token })
         if (!user) {
@@ -14,6 +14,7 @@ const auth = async(req, res, next) => {
         req.token = token
         next()
     } catch (error) {
+        if(error.message == "Cannot read property 'replace' of undefined") return res.status(401).send({error: "Token is missing"})
         if(error.message == "invalid signature") return res.status(401).send({error: "Invalid signature"})
         if(error.message == "jwt malformed") return res.status(401).send({error: "JWT malformed"})
         else res.status(401).send({error: "Not authorized to access this resources"})
